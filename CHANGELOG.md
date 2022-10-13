2db5567b5d17158a47c0a4510bad7800bfedd46d
Aleksei Semenov (Akvelon INC) <v-asemenov@microsoft.com>
2022-10-13 14:30:59 +0200

    updated libs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+
+## 3.0.0
+* Migrated to ESlint
+* Packages update
+* Vulnerabilities fixes
+
 ## 2.3.1
 * Packages update
 * Vulnerabilities fixes
--- karma.conf.ts
+++ karma.conf.ts
@@ -52,7 +52,6 @@ module.exports = (config) => {
         },
         singleRun: true,
         plugins: [
-            "karma-remap-istanbul",
             "karma-coverage",
             "karma-typescript",
             "karma-webpack",
@@ -62,8 +61,6 @@ module.exports = (config) => {
             "karma-coverage-istanbul-reporter"
         ],
         files: [
-            "node_modules/jquery/dist/jquery.min.js",
-            "node_modules/jasmine-jquery/lib/jasmine-jquery.js",
             srcCssRecursivePath,
             srcRecursivePath,
             testRecursivePath,
